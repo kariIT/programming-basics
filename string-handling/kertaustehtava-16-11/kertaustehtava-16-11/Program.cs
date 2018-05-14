b351d48791984a3cc790b302064572e858a86488
Joona Heinikari <joona.heinikari@elisanet.fi>
2018-05-14 14:25:43 +0300

    kertaustehtava-16-11

---
+++ string-handling/kertaustehtava-16-11/kertaustehtava-16-11/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace kertaustehtava_16_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give numbers: ");
+            
+
+            int x = 0;
+            int y = 0;
+            int i = 0;
+            int sum = 0;
+
+            do
+            {
+                string input = Console.ReadLine();
+                if (i % 2 == 0)
+                {
+                    Console.Write("Give number: ");
+                    x = int.Parse(input);
+                    sum = sum + x;
+                }
+                else
+                {
+                    Console.Write("Give number: ");
+                    y = int.Parse(input);
+                    sum = sum + y;
+                }
+
+                i++;
+
+            } while (x != y);
+
+            Console.WriteLine($"Sum: {sum}");
+            Console.ReadLine();
+        }
+    }
+}
