ea91802683502cb70ec94ef6c547982c475c0978
Joona Heinikari <joona.heinikari@elisanet.fi>
2018-05-14 14:04:22 +0300

    kertaustehtava-array-function

---
+++ string-handling/kertaustehtava-array-function/kertaustehtava-array-function/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace kertaustehtava_array_function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new[] {2, 5, 10, 7, 3};
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                string format = Format(numbers[i]);
+                Console.WriteLine($"{format}");
+            }
+
+            Console.ReadLine();
+        }
+
+        static string Format(int x)
+        {
+            string result = String.Empty;
+            for (int i = 0; i < x; i++)
+            {
+                result += "*";
+            }
+
+            return result;
+        }
+    }
+}
